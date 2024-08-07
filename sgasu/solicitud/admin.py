fab655e8f371b4fd32c5cf1e71a9ccff9268794e
Star7-jpg <marcosconde612@gmail.com>
2024-08-07 08:31:46 -0600

    Cambio de estructura

--- sgasu/solicitud/admin.py
+++ sgasu/solicitud/admin.py
@@ -3,7 +3,7 @@ from .models import Request
 
 # Register your models here.
 class RequestAdmin(admin.ModelAdmin):
-    list_display = ['reason', 'name', 'school_enrollment', 'date']
-    list_filter = ['date']
+    list_display = ['fk_name', 'fk_classroom', 'date']
+
 
 admin.site.register(Request, RequestAdmin)
